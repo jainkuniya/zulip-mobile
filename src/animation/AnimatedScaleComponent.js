2cc42bf234c2add78a5647ea892c35d144007ac4
jainkuniya <jainkuniya@gmail.com>
2020-03-25 18:54:56 +0530

    done

--- src/animation/AnimatedScaleComponent.js
+++ src/animation/AnimatedScaleComponent.js
@@ -1,7 +1,7 @@
 /* @flow strict-local */
 import React, { PureComponent } from 'react';
 import type { Node as React$Node } from 'react';
-import { Animated, Easing } from 'react-native';
+import { Animated, Easing, View, Text } from 'react-native';
 
 import type { Style } from '../types';
 
@@ -18,31 +18,82 @@ type State = {|
 export default class AnimatedScaleComponent extends PureComponent<Props, State> {
   state = {
     visible: this.props.visible,
+    animatedValue: new Animated.Value(0),
+    widthAnimatedValue: new Animated.Value(0),
+    minWidth: 0,
+    minHeight: 0,
+    maxHeight: 200,
+    maxWidth: 10000,
   };
 
-  animatedValue = new Animated.Value(this.props.visible ? 1 : 0);
+  setMaxHeight = event => {
+    this.setState({
+      maxHeight: event.nativeEvent.layout.height,
+      maxWidth: event.nativeEvent.layout.width,
+    });
+  };
+
+  toggle = visible => {
+    const initialValue = visible
+      ? this.state.maxHeight + this.state.minHeight
+      : this.state.minHeight;
+    const finalValue = visible ? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
+
+    this.setState({
+      visible,
+    });
+
+    this.state.animatedValue.setValue(initialValue);
+    Animated.timing(this.state.animatedValue, {
+      toValue: finalValue,
+      duration: 300,
+    }).start();
+
+    const initialValueW = visible ? this.state.maxWidth + this.state.maxWidth : this.state.minWidth;
+    const finalValueW = visible ? this.state.minWidth : this.state.maxWidth + this.state.minWidth;
+
+    this.state.widthAnimatedValue.setValue(initialValueW);
+    Animated.timing(this.state.widthAnimatedValue, {
+      toValue: finalValueW,
+      duration: 300,
+    }).start();
+  };
+
+  componentDidMount() {
+    // this.toggle(true);
+  }
 
   componentWillReceiveProps(nextProps: Props) {
-    if (nextProps.visible) {
-      this.setState({ visible: true });
-    }
-    Animated.timing(this.animatedValue, {
-      toValue: nextProps.visible ? 1 : 0,
-      duration: 300,
-      useNativeDriver: true,
-      easing: Easing.elastic(),
-    }).start(() => this.setState({ visible: nextProps.visible }));
+    this.toggle(nextProps.visible);
   }
 
   render() {
     const { children, style } = this.props;
-    const { visible } = this.state;
+    // const animatedStyle = {
+    //   transform: [
+    //     {
+    //       scaleY: this.animatedValue.interpolate({
+    //         inputRange: [0, 25, 50, 75, 100],
+    //         outputRange: [-1, 0.5, 0.75, 0.9, 1],
+    //       }),
+    //     },
+    //   ],
+    //   // height: this.animatedValue,
+    //   // opacity: this.animatedValue,
+    //   // display: visible ? 'flex' : 'none',
+    // };
+
     const animatedStyle = {
-      transform: [{ scale: this.animatedValue }],
-      opacity: this.animatedValue,
-      display: visible ? 'flex' : 'none',
+      height: this.state.animatedValue,
+      width: this.state.widthAnimatedValue,
     };
 
-    return <Animated.View style={[animatedStyle, style]}>{children}</Animated.View>;
+    return (
+      <Animated.View style={[style, animatedStyle]}>
+        <View style={style} onLayout={this.setMaxHeight}>
+          {children}
+        </View>
+      </Animated.View>
+    );
   }
 }
